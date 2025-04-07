42d1ff85cf50c41fb4e0aa27cbda3811f3f09074
Vishalini <visha546lini@gmail.com>
2025-04-07 18:02:56 +0530

    done

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -14,10 +14,8 @@ const Login = () => {
     e.preventDefault();
     console.log('Email:', email);
     console.log('Password:', password);
-
-    // Simulate a successful login
     console.log('Login successful');
-    navigate('/'); // Redirect to home
+    navigate('/');
   };
 
   const handleSignUp = (e) => {
@@ -29,17 +27,19 @@ const Login = () => {
     console.log('Full Name:', fullName);
     console.log('Email:', email);
     console.log('Password:', password);
-
     console.log('Sign-up successful');
-    setIsSigningUp(false); // Go back to the login form
+    setIsSigningUp(false);
   };
 
   return (
     <div className="login-container">
       {isSigningUp ? (
         <form className="login-form" onSubmit={handleSignUp}>
-           <div className="bookmyshow-logo">
-          <img src="https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F1ec7711b-5527-46d7-84fa-acbb5ef0a0df_666x222.png" alt="BookMyShow Logo" />
+          <div className="bookmyshow-logo">
+            <img 
+              src="https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F1ec7711b-5527-46d7-84fa-acbb5ef0a0df_666x222.png" 
+              alt="BookMyShow Logo" 
+            />
           </div>
           <h2>Sign Up</h2>
           <div className="form-group">
@@ -92,8 +92,11 @@ const Login = () => {
         </form>
       ) : (
         <form className="login-form" onSubmit={handleLogin}>
-           <div className="bookmyshow-logo">
-          <img src="https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F1ec7711b-5527-46d7-84fa-acbb5ef0a0df_666x222.png" alt="BookMyShow Logo" />
+          <div className="bookmyshow-logo">
+            <img 
+              src="https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F1ec7711b-5527-46d7-84fa-acbb5ef0a0df_666x222.png" 
+              alt="BookMyShow Logo" 
+            />
           </div>
           <h2>Login</h2>
           <div className="form-group">
