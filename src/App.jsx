292a0b7a9c0d89791c1ee8e5f82fa8c059c7d2a2
Vishalini <visha546lini@gmail.com>
2025-12-26 00:50:29 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,63 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+// Components
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import Login from "./Components/Login";
+import EventCards from "./Components/EventCards";
+import GiftCards from "./Components/GiftCards";
+
+// Home components
 import Carousel from "./Carousel";
 import MovieCardCarousel from "./MovieCardCarousel";
 import MovieCarousel2 from "./MovieCarousel2";
 import LiveEvents from "./LiveEvents";
 import CarouselComponent from "./CarouselComponent";
 import Laughter from "./Laughter";
-import Footer from "./Components/Footer";
-import EventCards from "./Components/EventCards";
-import GiftCards from "./Components/GiftCards";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 
-// Create a new component that will use useLocation
+// Home Page Component
+const Home = () => (
+  <>
+    <Carousel />
+    <MovieCardCarousel />
+    <MovieCarousel2 />
+    <LiveEvents />
+    <CarouselComponent />
+    <Laughter />
+  </>
+);
+
+// App content using useLocation
 const AppContent = () => {
   const location = useLocation();
 
   return (
-    <div>
-      {/* Conditionally render the Header if not on the login page */}
+    <>
+      {/* Header shown on all pages except login */}
       {location.pathname !== "/login" && <Header />}
 
-      {/* Define routes for the application */}
+      {/* Routes */}
       <Routes>
-        {/* Login Page Route */}
         <Route path="/login" element={<Login />} />
-
-        {/* Home Page Route with Carousel, Movie Carousels, and Live Events */}
-        <Route
-          path="/"
-          element={
-            <div>
-              <Carousel />
-              <MovieCardCarousel />
-              <MovieCarousel2 />
-              <LiveEvents />
-              <CarouselComponent />
-              <Laughter />
-            </div>
-          }
-        />
-
-        {/* EventCards Route */}
+        <Route path="/" element={<Home />} />
         <Route path="/event-cards" element={<EventCards />} />
-
-        {/* GiftCards Route */}
         <Route path="/gift-cards" element={<GiftCards />} />
       </Routes>
 
-      {/* Footer */}
+      {/* Footer always shown */}
       <Footer />
-    </div>
+    </>
   );
 };
 
-// Wrap the AppContent with Router
-const App = () => {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
-};
+// Main App wrapped in Router
+const App = () => (
+  <Router>
+    <AppContent />
+  </Router>
+);
 
 export default App;
